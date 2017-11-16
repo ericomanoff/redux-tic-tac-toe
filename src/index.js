c77353108ed691b48f1bfc16e5fccb8099757b06
Eric Omanoff <ericomanoff@gmail.com>
2017-11-15 19:49:14 -0700

    added react dev tools

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import registerServiceWorker from './registerServiceWorker';
 
-let store = createStore(select_square)
+let store = createStore(
+    select_square,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 ReactDOM.render(
     <Provider store={store}>
