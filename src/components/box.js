711b74ad7d7c2245897c2e7325c2544ff9df84ca
Eric Omanoff <ericomanoff@gmail.com>
2018-01-04 19:49:15 -0700

    added faces

--- src/components/box.js
+++ src/components/box.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Box = ({ onClick, team, id}) => {
     return(
-        <button onClick={onClick} className={team}> {team} </button>
+        <button onClick={onClick} className={team}>  </button>
     )
 }
 
